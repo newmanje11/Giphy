af1c2e9d4e1f8191772b7ac1fd3122036e3a6c1a
Jay <jevonte_n@outlook.com>
2018-08-08 17:21:44 -0400

    .

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,7 +1,10 @@
+var nintendos = ["Mario", "Luigi", "Princess Peach", "Bowser", "Princess Daisy", "WaLuigi", "Wario", "Link", "Zelda", "Pikachu","Charizard", "Marth" , "Roy" , "Lucina",
+ "Toad", "Bowser Jr", "Meta Knight" , "Kirby", "King Dedede", "King L. Rool", "Diddy Kong", "Tiy Kong", "Donkey Kong", "Little Mac", "Captian Olimar", "Wolf O'Donnell",
+  "Pauline", "Toadette", "Birdo", "Shulk", "Mr. Game & Watch", "Roy", "Dark Samus", "Falco Lombardi", "Rosalina", "Pit", "Samus Aran", "Captain Falcon", "Mewtwo", "Ridley", "Master Hand", "Wii Fit Trainer"]
+
 $(document).ready(function() {
     
 // make an array with the starting buttons
-var nintendos = ["Mario", "Luigi", "Princess Peach", "Bowser", "Princess Daisy", "WaLuigi", "Wario", "Link", "Zelda", "Pikachu","Charizard", "Marth" , "Roy" , "Lucina"];
 
 function getGifButtons () {
 
@@ -11,10 +14,11 @@ function getGifButtons () {
 for (i = 0; i < nintendos.length; i++) {
     var gifsButtons = $("<button>");
     gifsButtons.addClass("nintendo");
+    gifsButtons.addClass("btn btn-danger");
     gifsButtons.attr("data-name", nintendos[i]);
     gifsButtons.text(nintendos[i]);
     $("#buttons").append(gifsButtons);
-    console.log(nintendos[i]);
+    
 
     }
 }
@@ -23,7 +27,9 @@ getGifButtons();
 //add a new button 
 function newButtons() {
    
-        $("#submit").on("click", function(){
+        $("#submit").on("click", function(event){
+
+        event.preventDefault();
         var nintendo = $("#character-input").val().trim();
         if (nintendo == ""){
           return false; // added so there cannot add a blank button
@@ -39,31 +45,33 @@ function newButtons() {
     newButtons();
 
     function showGifs(){
-        var nintendo = $(this).attr("data-name");
 
-        var apiKey = "LhYtlLApHGVGRXZxUGH6MrjPhKiy5R3p";
+        var nintendos = $(".nintendo").attr("data-name")
+         
 
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + nintendos + "&api_key='" + apiKey +"'&limit=10";
+        var apiKey = "&api_key=LhYtlLApHGVGRXZxUGH6MrjPhKiy5R3p";
+
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ nintendos + "&api_key=LhYtlLApHGVGRXZxUGH6MrjPhKiy5R3p&limit=10"
         console.log(queryURL); // displays the constructed url
+        
         $.ajax({
             url: queryURL,
-            method: 'GET'
-        })
-        .done(function(response) {
-            console.log(response); // console test to make sure something returns
-            $("#image").empty(); // erasing anything in this div id so that it doesnt keep any from the previous click
+            method: "GET"
+          }).then(function(response) {
+
+            console.log(response.data); // console test to make sure something returns
+            
+            $("#info").empty(); // erasing anything in this div id so that it doesnt keep any from the previous click
             var results = response.data; //shows results of gifs
-            if (results == ""){
-              alert("There isn't a gif for this selected button");
-            }
+            
             for (var i=0; i<results.length; i++){
     
                 var gifDiv = $("<div>"); //div for the gifs to go inside
                 gifDiv.addClass("gifDiv");
-                // pulling rating of gif
+                // gets rating of gif
                 var gifRating = $("<p>").text("Rating: " + results[i].rating);
                 gifDiv.append(gifRating);
-                // pulling gif
+                // gets gif
                 var gifImage = $("<img>");
                 gifImage.attr("src", results[i].images.fixed_height_small_still.url); // still image stored into src of image
                 gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); // still image
@@ -71,12 +79,15 @@ function newButtons() {
                 gifImage.attr("data-state", "still"); // set the image state
                 gifImage.addClass("image");
                 gifDiv.append(gifImage);
-                // pulling still image of gif
-                // adding div of gifs to gifsView div
-                $("#images").prepend(gifDiv);
+                
+                $("#info").prepend(gifDiv);
             }
+            
         });
+        
+        
     }
+    showGifs();
 
 // getGifButtons()
 // var APIKey = "LhYtlLApHGVGRXZxUGH6MrjPhKiy5R3p";
